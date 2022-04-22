be8a0f462413b54110d4a41643a8878d597f94e9
Lamie2 <100703059+Lamie2@users.noreply.github.com>
2022-04-22 11:49:10 +0400

    Add files via upload

---
+++ StadionReservTask/Data/DAL/StadionDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StadionReservTask.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StadionReservTask.Data.DAL
+{
+    class StadionDbContext:DbContext
+    {
+        public DbSet<Stadion> Stadions { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-2VCD5FN;Database=StadionReserv;Trusted_Connection=TRUE;")
+        }
+    }
+}
